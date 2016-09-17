33ba4f690f1143a7b5feecd6a74da8cfdd77e962
Nick Loman <n.j.loman@bham.ac.uk>
2016-09-17 23:19:36 +0000

    ability to use json db

---
+++ scripts/runs.py
@@ -0,0 +1,28 @@
+
+import csv
+import sys
+from collections import defaultdict
+import json
+
+def get_runs_csv(fn):
+	runs = defaultdict(defaultdict)
+	reader = csv.reader(open(fn), delimiter='\t', dialect='excel-tab')
+	lastrun = ''
+	for row in reader:
+		if row[11] != '':
+			lastrun = row[11]
+
+		if row[4] not in runs[lastrun]:
+			runs[lastrun][row[4]] = list()
+		runs[lastrun][row[4]].append(row[3])
+	return runs
+
+def get_runs():
+    runs = defaultdict(defaultdict)
+    runsamples = json.load(open('runsamples.json'))
+    for sample in runsamples['data']:
+        if not sample['sample_id'] in runs[sample['Run::run_name']]:
+            runs[sample['Run::run_name']][sample['sample_id']] = list()
+
+        runs[sample['Run::run_name']][sample['sample_id']].append(sample['barcode_id'])
+    return runs
