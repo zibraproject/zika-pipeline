e1e2d327c9d387982bcd5a156c518b98cf67bde4
Nick Loman <n.j.loman@bham.ac.uk>
2016-09-17 23:20:52 +0000

    driver script

--- scripts/combineruns.py
+++ scripts/combineruns.py
@@ -2,21 +2,21 @@
 import csv
 import sys
 from collections import defaultdict
+#from runs import get_runs
+import json
 
 runs = defaultdict(defaultdict)
-reader = csv.reader(open(sys.argv[1]), delimiter='\t', dialect='excel-tab')
-lastrun = ''
-for row in reader:
-	if row[9] != '':
-		lastrun = row[9]
 
-	if row[4] not in runs[lastrun]:
-		runs[lastrun][row[4]] = list()
-	runs[lastrun][row[4]].append(row[3])
+runsamples = json.load(open('runsamples.json'))
+for sample in runsamples['data']:
+    if not sample['sample_id'] in runs[sample['Run::run_name']]:
+        runs[sample['Run::run_name']][sample['sample_id']] = list()
 
-for run, samplelookup in runs.iteritems():
-	print "# %s" % ( run , )
+    runs[sample['Run::run_name']][sample['sample_id']].append(sample['barcode_id'])
 
-	for s, barcodes in samplelookup.iteritems():
-		print "variants.sh %s ../zika-pipeline/metadata/v2.amplicons.bed %s" % (s, " ".join(barcodes))
-		print "margin_cons.py ../refs/Zika_FP.fasta %s.vcf %s.trimmed.sorted.bam a > %s.consensus.fasta" % (s, s, s)
+for run, samples in runs.iteritems():
+	print "cd %s" % (run,)
+	#print "go_zika.sh ../newdata/%s/downloads/pass %s" % (run, sys.argv[2])
+	for sample, barcodes in samples.iteritems():
+		print "variants.sh %s %s %s" % (sample, sys.argv[1], " ".join(barcodes))
+	print "cd .."
