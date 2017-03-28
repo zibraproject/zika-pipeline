60c01271b03e27ce34a6e7c84350cc3ee005fb14
Nick Loman <n.j.loman@bham.ac.uk>
2017-03-28 12:52:00 +0000

    nanopolish 1d version

---
+++ scripts/fast5_to_consensus_template.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -x
+set -e
+
+schema=$1
+sample=$2
+directory=$3
+
+nanopolish extract --type template ${directory} > ${sample}.fasta
+
+ref=/zibra/zika-pipeline/schemes/${schema}/V1/${schema}.reference.fasta
+bed=/zibra/zika-pipeline/schemes/${schema}/V1/${schema}.scheme.bed
+
+# Takes a $sample.fasta file full of nanopolish reads, ie
+# nanopolish extract --type 2d /data > $sample.fasta
+# Files are written to working directory
+
+# 2) copy the r9.4 model files into current directory
+#cp -f /zibra/models/new_fast_template.5mers.model .
+#cp -f /zibra/models/new_fast_template.model .
+#cp -f /zibra/models/new_models.fofn .
+
+# 3) index the ref & align with bwa
+bwa index $ref
+bwa mem -xont2d $ref $sample.fasta | samtools view -bS - | samtools sort -o $sample.sorted.bam -
+samtools index $sample.sorted.bam
+
+# 4) trim the alignments to the primer start sites and normalise the coverage to save time
+align_trim.py --start --normalise 100 $bed --report $sample.alignreport.txt < $sample.sorted.bam 2> $sample.alignreport.er | samtools view -bS - | samtools sort -T $sample - -o $sample.trimmed.sorted.bam
+align_trim.py --normalise 100 $bed --report $sample.alignreport.txt < $sample.sorted.bam 2> $sample.alignreport.er | samtools view -bS - | samtools sort -T $sample - -o $sample.primertrimmed.sorted.bam
+samtools index $sample.trimmed.sorted.bam
+samtools index $sample.primertrimmed.sorted.bam
+
+#covplot.R $sample.alignreport.txt
+
+# 6) do variant calling using the raw signal alignment
+nanopolish variants --progress -t 16 --reads $sample.fasta -o $sample.vcf -b $sample.trimmed.sorted.bam -g $ref -vv -w "`nanopolish_header.py $ref`" --snps --ploidy 1
+nanopolish variants --progress -t 16 --reads $sample.fasta -o $sample.primertrimmed.vcf -b $sample.primertrimmed.sorted.bam -g $ref -vv -w "`nanopolish_header.py $ref`" --snps --ploidy 1
+
+# 7) variant frequency plot
+vcfextract.py ${sample} > ${sample}.variants.tab
+
+# 8) filter the variants and produce a consensus
+margin_cons.py $ref $sample.vcf $sample.trimmed.sorted.bam a > $sample.consensus.fasta
