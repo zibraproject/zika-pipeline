6ad82d010895341235792fe52ab4a5c9ec6795b8
Nick Loman <n.j.loman@bham.ac.uk>
2017-03-28 12:49:48 +0000

    support nanopolish 0.6

--- scripts/fast5_to_consensus.sh
+++ scripts/fast5_to_consensus.sh
@@ -15,9 +15,9 @@ bed=/zibra/zika-pipeline/schemes/${schema}/V1/${schema}.scheme.bed
 # Files are written to working directory
 
 # 2) copy the r9.4 model files into current directory
-cp -f /zibra/models/new_fast_template.5mers.model .
-cp -f /zibra/models/new_fast_template.model .
-cp -f /zibra/models/new_models.fofn .
+#cp -f /zibra/models/new_fast_template.5mers.model .
+#cp -f /zibra/models/new_fast_template.model .
+#cp -f /zibra/models/new_models.fofn .
 
 # 3) index the ref & align with bwa
 bwa index $ref
@@ -32,13 +32,9 @@ samtools index $sample.primertrimmed.sorted.bam
 
 #covplot.R $sample.alignreport.txt
 
-# 5) align the nanopore raw signal against the reference using the previous alignments as a guide
-nanopolish eventalign -vvvv -t 16 --models-fofn new_models.fofn --reads $sample.fasta -b $sample.trimmed.sorted.bam -g $ref --sam | samtools view -bS - | samtools sort -T $sample.tmp - -o $sample.np.sorted.bam
-samtools index $sample.np.sorted.bam
-
 # 6) do variant calling using the raw signal alignment
-nanopolish variants --progress -t 16 --reads $sample.fasta -o $sample.vcf -b $sample.trimmed.sorted.bam -e $sample.np.sorted.bam -g $ref -vv -w "`nanopolish_header.py $ref`" --snps --models-fofn new_models.fofn
-nanopolish variants --progress -t 16 --reads $sample.fasta -o $sample.primertrimmed.vcf -b $sample.primertrimmed.sorted.bam -e $sample.np.sorted.bam -g $ref -vv -w "`nanopolish_header.py $ref`" --snps --models-fofn new_models.fofn
+nanopolish variants --progress -t 16 --reads $sample.fasta -o $sample.vcf -b $sample.trimmed.sorted.bam -g $ref -vv -w "`nanopolish_header.py $ref`" --snps --ploidy 1
+nanopolish variants --progress -t 16 --reads $sample.fasta -o $sample.primertrimmed.vcf -b $sample.primertrimmed.sorted.bam -g $ref -vv -w "`nanopolish_header.py $ref`" --snps --ploidy 1
 
 # 7) variant frequency plot
 vcfextract.py ${sample} > ${sample}.variants.tab
